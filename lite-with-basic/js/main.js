14442f91b9058dcc20ffefde2c980154672f1f5d
Vasil Ilyuhin <v.ilyuhin@gmail.com>
2024-09-01 17:26:23 +0300

    deploy lite with basic page

---
+++ lite-with-basic/js/main.js
@@ -0,0 +1,287 @@
+var steps = Array.from(document.querySelectorAll(".scr_quiz__step"));
+var len = steps.length;
+var progress = document.querySelector(".progress-line");
+
+var moveProgressLine = () => {
+    var active_step = steps.filter((item) =>
+        item.classList.contains("active")
+    )[0];
+
+    progress.style.width = `${(active_step.dataset.step / len) * 100}%`;
+};
+
+var initGetBtns = (interview, steps) => {
+    var get_btn = document.querySelector(".scr_quiz__get_btn");
+    var header = document.querySelector(".scr_header");
+
+    if (!get_btn || !interview || steps.length < 1) return;
+
+    [get_btn].forEach((item) =>
+        item.addEventListener("click", () => {
+            interview.classList.add("active");
+            header.classList.add("fixed");
+            document.body.classList.add("lock");
+            moveProgressLine();
+        })
+    );
+};
+
+var initCloseBtns = (interview, steps) => {
+    var close_btns = Array.from(document.querySelectorAll(".quiz-close"));
+    var confirm = document.querySelector(".scr_quiz__confirm");
+    var header = document.querySelector(".scr_header");
+    var get_btn = document.querySelector(".scr_quiz__get_btn");
+
+    if (close_btns.length < 1) return;
+
+    close_btns.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            interview.style.display = "none";
+            setTimeout(() => (interview.style.display = "block"), 500);
+            get_btn.setAttribute("disabled", "disabled");
+            interview.classList.remove("active");
+            steps.forEach((step) =>
+                step.dataset.step === "1"
+                    ? step.classList.add("active")
+                    : step.classList.remove("active")
+            );
+
+            confirm.classList.remove("active");
+            header.classList.remove("fixed");
+            document.body.classList.remove("lock");
+            progress.style.display = "none";
+            progress.style.width = `0%`;
+            setTimeout(() => {
+                progress.style.display = "block";
+                get_btn.removeAttribute("disabled");
+            }, 500);
+        });
+    });
+};
+
+var moveStep = (btn, dir) => {
+    var current_step = btn.closest(".scr_quiz__step");
+    var direction =
+        dir === "next" ? "nextElementSibling" : "previousElementSibling";
+    var next_step = current_step[direction];
+    current_step.classList.remove("active");
+    next_step.classList.add("active");
+    moveProgressLine();
+};
+
+var initRadioBtns = (interview, steps) => {
+    var radio_btns = Array.from(
+        document.querySelectorAll(".scr_quiz__label_radio input")
+    );
+
+    if (radio_btns.length < 1) return;
+
+    const NEXT = "next";
+
+    radio_btns.forEach((btn) => {
+        btn.addEventListener("click", () =>
+            setTimeout(() => moveStep(btn, NEXT), 500)
+        );
+    });
+};
+
+var initBackwardBtns = () => {
+    var backward_btns = Array.from(document.querySelectorAll(".quiz-backward"));
+
+    if (backward_btns.length < 1) return;
+
+    const PREV = "prev";
+
+    backward_btns.forEach((btn) => {
+        btn.addEventListener("click", () => moveStep(btn, PREV));
+    });
+};
+
+var initForwardBtns = () => {
+    var forward_btns = Array.from(document.querySelectorAll(".quiz-forward"));
+
+    if (forward_btns.length < 1) return;
+
+    const NEXT = "next";
+
+    forward_btns.forEach((btn) => {
+        btn.addEventListener("click", () =>
+            setTimeout(() => moveStep(btn, NEXT), 500)
+        );
+    });
+};
+
+var formSubmit = (steps) => {
+    var form = document.querySelector(".scr_quiz__interview");
+
+    if (!form) return;
+
+    var inputs = Array.from(form.elements);
+
+    var required_inputs = inputs.filter((input) =>
+        input.classList.contains("required")
+    );
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        required_inputs.forEach((input) => {
+            if (!input.value) {
+                input.classList.add("error");
+            } else {
+                input.classList.remove("error");
+            }
+        });
+
+        if (
+            required_inputs.some((input) => input.classList.contains("error"))
+        ) {
+            return;
+        }
+
+        form.reset();
+        steps.forEach((step) => step.classList.remove("active"));
+        var confirm = document.querySelector(".scr_quiz__confirm");
+        confirm.classList.add("active");
+    });
+};
+
+// var initMagneticButtons = () => {
+//     var area = document.querySelector(".scr_quiz__wrapper");
+//     var btn = document.querySelector(".scr_quiz__get_btn");
+
+//     if (!area || !btn) return;
+
+//     var parallaxIt = (e, target, movement = 1) => {
+//         var boundingRect = area.getBoundingClientRect();
+//         var relX = e.pageX - boundingRect.left;
+//         var relY = e.pageY - boundingRect.top;
+//         var scrollTop =
+//             window.pageYOffset || document.documentElement.scrollTop;
+
+//         requestAnimationFrame(() => {
+//             target.style.transform = `translate(${
+//                 (relX - boundingRect.width / 2 + target.scrollWidth / 1.5) *
+//                 movement
+//             }px, ${
+//                 (relY -
+//                     boundingRect.height / 2 -
+//                     scrollTop +
+//                     target.scrollHeight / 1.5) *
+//                 movement
+//             }px)`;
+//         });
+//     };
+
+//     var callParallax = (e) => {
+//         btn.classList.add("active");
+//         parallaxIt(e, btn);
+//     };
+
+//     var leaveHandler = () => {
+//         btn.style.transform = `translate(0px, 0px)`;
+//         btn.classList.remove("active");
+//     };
+
+//     if (window.innerWidth > 767) {
+//         area.addEventListener("mousemove", callParallax);
+//         area.addEventListener("mouseleave", leaveHandler);
+//     }
+
+//     // toggle header handlers by resize
+//     var mqMin767 = window.matchMedia("(min-width: 767px)");
+
+//     var handleMQ = (e) => {
+//         if (e.matches) {
+//             area.addEventListener("mousemove", callParallax);
+//             area.addEventListener("mouseleave", leaveHandler);
+//         } else {
+//             area.removeEventListener("mousemove", callParallax);
+//             area.removeEventListener("mouseleave", leaveHandler);
+//         }
+//     };
+
+//     mqMin767.addEventListener("change", handleMQ);
+// };
+
+var initContacts = () => {
+    var buttons = document.querySelectorAll(".contacts__copy");
+
+    if (buttons.length < 1) return;
+
+    buttons.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            var link = btn.previousElementSibling;
+            if (!link) return;
+
+            const value = link.textContent;
+            navigator.clipboard.writeText(value);
+        });
+    });
+};
+
+var doCreateMapScript = (cb) => {
+    setTimeout(function () {
+        var script = document.createElement("script");
+        script.async = false;
+        script.src = "https://api-maps.yandex.ru/2.1/?apikey=key&lang=ru_RU";
+        document.body.appendChild(script);
+        script.onload = () => cb();
+    }, 2000);
+};
+
+var initMap = () => {
+    var init = () => {
+        var coords = [45.054809, 38.970974];
+        var mark_link = "images/mark.svg";
+
+        if (ymaps) {
+            var map = new ymaps.Map("map", {
+                center: coords,
+                zoom: 17,
+            });
+
+            var placemark = new ymaps.Placemark(
+                coords,
+                {},
+                {
+                    iconLayout: "default#image",
+                    iconImageHref: mark_link,
+                    iconImageSize: [100, 100],
+                    iconImageOffset: [-60, -80],
+                }
+            );
+
+            map.controls.remove("geolocationControl"); // удаляем геолокацию
+            map.controls.remove("searchControl"); // удаляем поиск
+            map.controls.remove("trafficControl"); // удаляем контроль трафика
+            map.controls.remove("typeSelector"); // удаляем тип
+            map.controls.remove("fullscreenControl"); // удаляем кнопку перехода в полноэкранный режим
+            // map.controls.remove("zoomControl"); // удаляем контрол зуммирования
+            map.controls.remove("rulerControl"); // удаляем контрол правил
+            // map.behaviors.disable(["scrollZoom"]); // отключаем скролл карты (опционально)
+
+            map.geoObjects.add(placemark);
+        }
+    };
+
+    ymaps.ready(init);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    var interview = document.querySelector(".scr_quiz__interview");
+    var steps = Array.from(document.querySelectorAll(".scr_quiz__step"));
+
+    initGetBtns(interview, steps);
+    initCloseBtns(interview, steps);
+    initRadioBtns(interview, steps);
+    initBackwardBtns(interview, steps);
+    initForwardBtns(interview, steps);
+    formSubmit(steps);
+    initContacts();
+
+    var map = document.getElementById("map");
+    map && doCreateMapScript(initMap);
+
+    // initMagneticButtons();
+});
